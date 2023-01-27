d910e8bcc39ddc0d3490052efc24f728a6f06dc1
henri <henri@DESKTOP-6O1UG2H>
2023-01-27 20:29:26 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjektObjektorineteradprogrammering;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjektObjektorineteradprogrammering;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
